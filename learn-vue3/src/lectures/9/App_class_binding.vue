7d689d6564de412277f91e3e1e101f96369e5f33
이준혁 <ballack02@naver.com>
2023-10-05 18:03:51 +0900

    class와 style 바인딩

---
+++ learn-vue3/src/lectures/9/App_class_binding.vue
@@ -0,0 +1,61 @@
+<template>
+	<div>
+		<div
+			class="text"
+			:class="[isActive ? 'active-class' : 'class', errorClass, classObject]"
+		>
+			텍스트 입니다.
+		</div>
+		<button @click="toggle">toggle</button>
+		<button @click="hasError = !hasError">toggleError</button>
+	</div>
+</template>
+
+<script>
+import { computed, reactive, ref } from 'vue';
+
+export default {
+	setup() {
+		const isActive = ref(true);
+		const hasError = ref(false);
+
+		// const classObject = reactive({
+		// 	active: true,
+		// 	error: false,
+		// });
+
+		const classObject = computed(() => {
+			return {
+				active: true,
+				error: true,
+				blue: true,
+			};
+		});
+
+		const activeClass = ref('active');
+		const errorClass = ref('error');
+
+		const toggle = () => {
+			isActive.value = !isActive.value;
+		};
+
+		return {
+			classObject,
+			isActive,
+			hasError,
+			toggle,
+			activeClass,
+			errorClass,
+		};
+	},
+};
+</script>
+
+<style scoped>
+.active {
+	font-weight: 900;
+}
+.error {
+	color: orange;
+}
+</style>
