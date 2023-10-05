24667d545c858fa8f27cdde4451776785c778083
이준혁 <ballack02@naver.com>
2023-10-05 15:22:43 +0900

    reactive, ref 함수 차이 ref 함수 object 대입과 array 대입의 차이 toRefs와 toRef의 사용방법 readonly api 사용방법

---
+++ learn-vue3/src/lectures/7/App_ref.vue
@@ -0,0 +1,30 @@
+<template>
+	<div>
+		<p>{{ message }}</p>
+		<button @click="addMessage">add Click</button>
+	</div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+	setup() {
+		let message = ref('Hello Vue3!');
+		const addMessage = () => {
+			message.value = message.value + '!';
+		};
+
+		console.log(message);
+		console.log('message: ', message.value);
+		console.log('message typeof: ', typeof message.value);
+
+		return {
+			message,
+			addMessage,
+		};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
