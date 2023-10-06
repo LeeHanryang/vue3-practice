69634041af00d4d6359453d37b9f5f3e84efe3c8
이준혁 <ballack02@naver.com>
2023-10-06 17:36:46 +0900

    directive, memo 사용방법

--- learn-vue3/src/App.vue
+++ learn-vue3/src/App.vue
@@ -1,47 +1,11 @@
 <template>
-	<div>
-		<ul>
-			<template v-for="(item, index) in evenItems" :key="item.id">
-				<!-- <li v-if="item.id % 2 === 0"> -->
-				<li>ID: {{ item.id }}, Index : {{ index }}, {{ item.message }}</li>
-			</template>
-		</ul>
-		<hr />
-		<ul>
-			<li v-for="(value, key, index) in myObject" :key="key">
-				{{ index }}-{{ key }}-{{ value }}
-			</li>
-		</ul>
-	</div>
+	<div></div>
 </template>
 
 <script>
-import { computed, reactive } from 'vue';
-
 export default {
 	setup() {
-		const items = reactive([
-			{ id: 1, message: 'Java' },
-			{ id: 2, message: 'Vue3' },
-			{ id: 3, message: 'Python' },
-			{ id: 4, message: 'C++' },
-			{ id: 5, message: 'JavaScript' },
-			{ id: 6, message: 'HTML' },
-		]);
-
-		const evenItems = computed(() => items.filter(item => item.id % 2 === 0));
-
-		const myObject = reactive({
-			title: '제목',
-			author: '홍길동',
-			publishedAt: '2016-01-01',
-		});
-
-		return {
-			items,
-			evenItems,
-			myObject,
-		};
+		return {};
 	},
 };
 </script>
