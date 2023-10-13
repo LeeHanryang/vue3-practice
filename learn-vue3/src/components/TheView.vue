d9e3da48920be6c49f6971baecdb62e96a0b066d
이준혁 <ballack02@naver.com>
2023-10-13 11:13:30 +0900

    component 분할, 작명 규칙, 권장 규칙

---
+++ learn-vue3/src/components/TheView.vue
@@ -0,0 +1,46 @@
+<template>
+	<div>
+		<main>
+			<div class="container py-4">
+				<div class="row g-3">
+					<div class="col col-4">
+						<!-- <AppCard /> -->
+						<app-card />
+					</div>
+					<div class="col col-4">
+						<AppCard />
+					</div>
+					<div class="col col-4">
+						<AppCard />
+					</div>
+					<div class="col col-4">
+						<AppCard />
+					</div>
+					<div class="col col-4">
+						<AppCard />
+					</div>
+					<div class="col col-4">
+						<AppCard />
+					</div>
+				</div>
+			</div>
+		</main>
+	</div>
+</template>
+
+<script>
+console.log('App Card Module()');
+import AppCard from '@/components/AppCard.vue';
+
+export default {
+	components: {
+		AppCard,
+	},
+	setup() {
+		console.log('App Card Setup()');
+		return {};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
