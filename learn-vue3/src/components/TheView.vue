f40cb65f721e58aa23dc7c14e1b9123c59fcd004
이준혁 <ballack02@naver.com>
2023-10-16 15:53:25 +0900

    props 사용법, 규칙

--- learn-vue3/src/components/TheView.vue
+++ learn-vue3/src/components/TheView.vue
@@ -3,24 +3,14 @@
 		<main>
 			<div class="container py-4">
 				<div class="row g-3">
-					<div class="col col-4">
-						<!-- <AppCard /> -->
-						<app-card />
-					</div>
-					<div class="col col-4">
-						<AppCard />
-					</div>
-					<div class="col col-4">
-						<AppCard />
-					</div>
-					<div class="col col-4">
-						<AppCard />
-					</div>
-					<div class="col col-4">
-						<AppCard />
-					</div>
-					<div class="col col-4">
-						<AppCard />
+					<div v-for="post in posts" :key="post.id" class="col col-4">
+						<AppCard
+							:title="post.title"
+							:contents="post.contents"
+							:type="post.type"
+							:is-like="post.isLike"
+							@toggle-like="post.isLike = !post.isLike"
+						/>
 					</div>
 				</div>
 			</div>
@@ -30,13 +20,58 @@
 
 <script>
 import AppCard from '@/components/AppCard.vue';
+import { reactive } from 'vue';
 
 export default {
 	components: {
 		AppCard,
 	},
 	setup() {
-		return {};
+		const posts = reactive([
+			{
+				id: '1',
+				title: '제목1',
+				contents: '내용1',
+				isLike: false,
+				type: 'news',
+			},
+			{
+				id: '2',
+				title: '제목2',
+				contents: '내용2',
+				isLike: true,
+				type: 'news',
+			},
+			{
+				id: '3',
+				title: '제목3',
+				contents: '내용3',
+				isLike: true,
+				type: 'news',
+			},
+			{
+				id: '4',
+				title: '제목4',
+				contents: '내용4',
+				isLike: true,
+				type: 'news',
+			},
+			{
+				id: '5',
+				title: '제목5',
+				contents: '내용5',
+				isLike: false,
+				type: 'notice',
+			},
+			{
+				id: '6',
+				title: '제목6',
+				contents: '내용6',
+				isLike: false,
+				type: 'notice',
+			},
+		]);
+		return { posts };
 	},
 };
 </script>
